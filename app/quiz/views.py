6fad48f439fe00bf72b552ea203118e936d6c164
Alex Siqueira Lacerda <lequizi@gmail.com>
2024-04-17 09:18:03 -0300

    api views setup

--- app/quiz/views.py
+++ app/quiz/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from ninja import NinjaAPI
+from django.core.handlers.wsgi import WSGIRequest
 
-# Create your views here.
+from .models import Question
+from .schemas import AnswerIn, AnswerOut, QuestionOut, ErrorSchema
+from .questions import QuestionCategory, GPTQuestionGenerator, QuestionGenerator
+
+
+api = NinjaAPI()
+
+
+@api.get("/question", response={200: QuestionOut, 500: ErrorSchema})
+def get_question(request: WSGIRequest, category: QuestionCategory = 'random'):
+    question_generator = GPTQuestionGenerator()
+    
+    try:
+        question = question_generator.generate_question(category=category)
+    except:
+        return 500, {'message': "Sorry. We're currently unable to generate a question for you."}
+    
+    return question
+
+
+@api.post("/answer", response={200: AnswerOut, 404: ErrorSchema})
+def answer(request: WSGIRequest, answer: AnswerIn):
+    try:
+        question = Question.objects.get(uuid=answer.question_uuid)
+    except:
+        return 404, {'message': f"Sorry. We couldn't find a question with the uuid {answer.question_uuid}."}
+    
+    return { 'correct' : question.check_answer(answer=answer.answer) }
