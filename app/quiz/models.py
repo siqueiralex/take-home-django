9403588ca71ee15e5bf1e74a59dddcd218477126
Alex Siqueira Lacerda <lequizi@gmail.com>
2024-04-17 09:17:20 -0300

    adding question category and answer check

--- app/quiz/models.py
+++ app/quiz/models.py
@@ -4,6 +4,18 @@ from django.db import models
 
 
 class Question(models.Model):
+    CATEGORY = (
+        ('geography', 'Geography'),
+        ('science', 'Science'),
+    )
+    
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     question = models.TextField()
     answer = models.CharField(max_length=200)
+    category = models.CharField(
+        max_length=100,
+        choices=CATEGORY,
+    )
+    
+    def check_answer(self, answer:str):
+        return answer.strip().lower() == self.answer.strip().lower()
