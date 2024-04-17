cd30fafff597d5d01a8acfaefd2e7b80c4c69af5
Alex Siqueira Lacerda <lequizi@gmail.com>
2024-04-17 08:11:53 -0300

    creating model Question

--- app/quiz/models.py
+++ app/quiz/models.py
@@ -1,3 +1,9 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+
+class Question(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    question = models.TextField()
+    answer = models.CharField(max_length=200)
