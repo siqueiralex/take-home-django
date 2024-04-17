cb01b7a5b6bb1a43dff800ca4748bd533af78580
Alex Siqueira Lacerda <lequizi@gmail.com>
2024-04-17 09:45:31 -0300

    implementing GPT call

--- app/quiz/questions.py
+++ app/quiz/questions.py
@@ -16,6 +16,11 @@ class QuestionGenerator(ABC):
 
 class GPTQuestionGenerator(QuestionGenerator):
     
+    def __init__(self, gpt_model = "gpt-3.5-turbo", gpt_temperature = .8) -> None:
+        self.gpt_model = gpt_model
+        self.gpt_temperature = gpt_temperature
+        self.openai_client = OpenAI()
+    
     def generate_question(self, category:QuestionCategory) -> Question:
         # dealing with random
         if category == 'random':
@@ -24,11 +29,34 @@ class GPTQuestionGenerator(QuestionGenerator):
             category = random.choice(category_list)
         
         
-        ## Call GPT
+        system_message = '''
+        You are an assistant to creating Quiz questions.
+        You always create questions with 4 choices: A, B, C and D.
+        The questions you create always have only one correct choice.
+        Your response is always sent in the following structure:
+        [Question Statement]
+        A - Option A
+        B - Option B
+        C - Option C
+        D - Option D
+        Correct Answer: B
+        '''
+        
+        response = self.openai_client.chat.completions.create(
+            model = self.gpt_model,
+            temperature = self.gpt_temperature,
+            messages = [
+                {'role' : 'system', 'content': system_message.strip()},
+                {'role' : 'user', 'content': f"Create a simple question in the category: {category}"}
+            ],
+        )
+        
+        res_list = response.choices[0].message.content.split('Correct Answer:')
+        question = res_list[0].strip()
+        answer = res_list[1].strip()
         
         return Question.objects.create(
             category = category,
-            question = 'foo',
-            answer = 'foo'
+            question = question,
+            answer = answer
         )
-
